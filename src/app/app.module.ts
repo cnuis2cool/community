b1fbad5ea97c62596d55779e17118c1be7d677a3
PANUGANTI Srinivas <srinivas.panuganti@hexagonsi.com>
2018-04-03 17:22:45 +0530

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+
+import { OrdersPage } from '../pages/orders/orders';
+import { ProfilePage } from '../pages/profile/profile';
+import { HomePage } from '../pages/home/home';
+import { CategoryPage } from '../pages/category/category';
+import { SettingsPage } from '../pages/settings/settings';
+import { TabsPage } from '../pages/tabs/tabs';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    OrdersPage,
+    ProfilePage,
+    HomePage,
+    CategoryPage,
+    SettingsPage,
+    TabsPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    OrdersPage,
+    ProfilePage,
+    HomePage,
+    CategoryPage,
+    SettingsPage,
+    TabsPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ src/pages/tabs/tabs.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+import { OrdersPage } from '../orders/orders';
+import { ProfilePage } from '../profile/profile';
+import { HomePage } from '../home/home';
+import { CategoryPage } from '../category/category';
+import { SettingsPage } from '../settings/settings';
+
+@Component({
+  templateUrl: 'tabs.html'
+})
+export class TabsPage {
+
+  tab1Root = HomePage;
+  tab2Root = OrdersPage;
+  tab3Root = ProfilePage;
+  tab4Root = CategoryPage;
+  tab5Root = SettingsPage;
+
+  constructor() {
+
+  }
+}
